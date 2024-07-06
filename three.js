2b3c6495dfc316661716a2d78024fbada3c1a175
Nerz <adrian.dufac@gmail.com>
2024-07-06 09:24:39 +0200

    setUp another canvas for more tests

--- three.js
+++ three.js
@@ -1,6 +1,8 @@
 import * as THREE from "../node_modules/three/build/three.module.js";
 
 const threeCanvas = document.getElementById("three-canvas");
+const threeCanvasB = document.getElementById("three-canvas-b");
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -8,11 +10,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(threeCanvas.offsetWidth, threeCanvas.offsetHeight);
-
-threeCanvas.appendChild(renderer.domElement);
+attachSceneToCanvas(threeCanvas, renderer);
 
 // boxGemotry to create a cube
 const geometry = new THREE.BoxGeometry(1, 1, 1); // width height depth
@@ -25,6 +24,13 @@ camera.position.z = 5;
 
 //creating a render loop
 function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
+
+function attachSceneToCanvas(canvas, renderer) {
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+  canvas.appendChild(renderer.domElement);
+}
